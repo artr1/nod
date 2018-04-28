9a67a2ddfece2a8b7c5a1f1128a2c4f369ac705b
ArtR <38791778+artr1@users.noreply.github.com>
2018-04-28 15:19:45 +0300

    тест

--- app.js
+++ app.js
@@ -16,10 +16,13 @@ app.use(express.static(__dirname + "/public"));
 connection.connect();
 
 app.get("/", function(request, response){
-	connection.query('SELECT * FROM listeners', (err, row, fields)=>{
+	connection.query('SELECT * FROM programs', (err, row, fields)=>{
 		if (err) throw err;
 		console.log(row);
-		response.render("index.pug", {});
+		const RP = {
+			listener: row[0]
+		}
+		response.render("index.pug", RP);
 	});
 });
 
