a215ffaf3e157a70140bafbac9d03781b60548a3
ArtR <38791778+artr1@users.noreply.github.com>
2018-05-04 16:35:05 +0300

    04.05.2018_2

--- public/js/form.js
+++ public/js/form.js
@@ -1,13 +1,10 @@
 $("form").submit(function(e) {
 	e.preventDefault();//перехват события
 	var form = $(this);//текущая форма
-	//console.log(form.serialize())
 	$.ajax({
 		type: "POST",
 		url: form.attr("action"),//подставляется атрибут action
 		data: form.serialize(),//передаем все данные формы
-	//	dataType: "json",
-	//	contentType: "application/json",
 		success: function(data){
 			var html = '<tr id="row-'+data.id+'"><td><div class="alert-tb alert-primary" data-id="'+data.id+'">'+data.name+'</div></td><td width="16px"><input class="delete" dataid='+data.id+' type="image" src="img/delete.png" onclick="del('+data.id+')"></td><td width="16px"><input type="image" src="img/ed.png"></td></tr>';
 			$('table tbody').append(html);
