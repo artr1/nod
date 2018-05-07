851206831bbb5bc6d3675ea48923a7c3bf7786bc
ArtR <38791778+artr1@users.noreply.github.com>
2018-05-07 17:57:28 +0300

    07.05.2018

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const pug = require('pug');
 //const jsonParser = bodyParser.json();
 const mysql = require("mysql");
-const connection = mysql.createConnection({
+const connection = mysql.createPool({
 	host: "localhost",
 	user: "root",
 	password: "",
@@ -15,7 +15,6 @@ app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.json());//если данные пришли в json
 app.use(bodyParser.urlencoded({ extended:true }));//если данные пришли строкой
 
-connection.connect();
 
 app.get("/", function(req, res){
 	connection.query('SELECT * FROM programs', (err, row, fields)=>{
@@ -28,15 +27,23 @@ app.get("/", function(req, res){
 	});
 });
 
+// Пример экранирования
+// var post  = {id: 1, title: 'Hello MySQL'};
+// var query = connection.query('INSERT INTO posts SET ?', post, function (error, results, fields) {
+//   if (error) throw error;
+//   // Neat!
+// });
+// console.log(query.sql); // INSERT INTO posts SET `id` = 1, `title` = 'Hello MySQL'
+
 app.post("/user", function (req, res) {
 	if (req.body.programName) {
-		connection.query('INSERT INTO programs (program) VALUES ("'+req.body.programName+'")', (err, result)=>{
+		connection.query('INSERT INTO programs SET ?', {program: req.body.programName}, (err, result, fields)=>{
 			if (err) throw err;
 			res.json({id: result.insertId, name: req.body.programName});
-			//console.log(result);
 		});
 		}
 });
+
 
 app.post("/delete", function (req, res) {
 	connection.query('DELETE FROM programs WHERE id="'+req.body.dataid+'"', (err, result)=>{
--- public/js/form.js
+++ public/js/form.js
@@ -8,6 +8,7 @@ $("form").submit(function(e) {
 		success: function(data){
 			var html = '<tr id="row-'+data.id+'"><td><div class="alert-tb alert-primary" data-id="'+data.id+'">'+data.name+'</div></td><td width="16px"><input class="delete" dataid='+data.id+' type="image" src="img/delete.png" onclick="del('+data.id+')"></td><td width="16px"><input type="image" src="img/ed.png"></td></tr>';
 			$('table tbody').append(html);
+			$('form')[0].reset();
 			console.log(data);
 			},
 		});
@@ -25,4 +26,18 @@ function del(id) {
 			$("#row-"+id).remove();
 			},
 		});
+};
+
+function edit(id) {
+	console.log(id);
+	$("#row-"+id).replaceWith('<tr id="row-'+id+'"><td><div class="alert-tb alert-primary" data-id="'+id+'"></div></td><td width="16px"><input class="delete" dataid='+id+' type="image" src="img/delete.png" onclick="del('+id+')"></td><td width="16px"><input type="image" src="img/save.png"></td></tr>');
+	// $.ajax({
+	// 	type: "POST",
+	// 	url: "/delete",
+	// 	data: {dataid: id},
+	// 	success: function(data){
+	// 		console.log(data.success);
+	// 		$("#row-"+id).remove();
+	// 		},
+	// 	});
 };
